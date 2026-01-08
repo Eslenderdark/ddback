6a8f152bd1a7533a1a2675b5eb0ab9296dadc755
V3\victo <vcantero06@colegiosantamonica.eu>
2026-01-08 16:15:50 +0100

    bbdd

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'dddb_30fk_user',
-  password: 'T6uC79Lxh6F6uyTlAHfu2eLv9R0X4Yxf',
-  host: 'dpg-d4tevce3jp1c73ee0vc0-a.frankfurt-postgres.render.com',
+  user: 'dungeonsbbdd_user',
+  password: '6OpbIzzxopuW1Z48WfeI9X5Sz7fah7n3',
+  host: 'dpg-d5fsf5c9c44c738rhsrg-a.frankfurt-postgres.render.com',
   port: 5432,
-  database: 'dddb_30fk',
+  database: 'dungeonsbbdd',
   ssl: { rejectUnauthorized: false }
 });
 
