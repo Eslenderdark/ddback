c6caaebf85026019b367d9c8952a4d9612ccfca6
OscarEmpirico <theoscardream@outlook.es>
2026-02-08 12:38:33 +0100

    cambio bdd

--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'dungeonsbbdd_user',
-  password: '6OpbIzzxopuW1Z48WfeI9X5Sz7fah7n3',
-  host: 'dpg-d5fsf5c9c44c738rhsrg-a.frankfurt-postgres.render.com',
+  user: 'ddbb_wcpt_user',
+  password: '3VApO1utjDl6uPLfEJNLI3EyhM8f5LTB',
+  host: 'dpg-d6472jpr0fns73c27ph0-a.frankfurt-postgres.render.com',
   port: 5432,
-  database: 'dungeonsbbdd',
+  database: 'ddbb_wcpt',
   ssl: { rejectUnauthorized: false }
 });
 
